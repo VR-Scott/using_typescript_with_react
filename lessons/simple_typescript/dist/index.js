18cfed7fe6a7e978bd88baac3544a7498b4bbe09
VR-Scott <vscott@student.wethinkcode.co.za>
2020-04-11 18:46:17 +0200

    .\node_modules\.bin\concurrently QTtsc -wQT QTnpx nodemon -w dist -q dist\index.jsQT to concurrently run.

--- lessons/simple_typescript/dist/index.js
+++ lessons/simple_typescript/dist/index.js
@@ -2,6 +2,6 @@
 function multiply(a, b) {
     return a * b;
 }
-const a = 5;
-const b = 3;
+const a = 6;
+const b = 7;
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
--- lessons/simple_typescript/src/index.ts
+++ lessons/simple_typescript/src/index.ts
@@ -2,7 +2,7 @@ function multiply(a: number, b: number): number {
     return a * b;
 }
 
-const a = 5;
-const b = 5;
+const a = 6;
+const b = 7;
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
