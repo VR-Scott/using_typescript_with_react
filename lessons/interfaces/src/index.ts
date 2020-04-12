b411d165840d106e1f5354ad9f7c5ea8597e319d
VR-Scott <vscott@student.wethinkcode.co.za>
2020-04-12 15:50:50 +0200

    finished interfaces

--- lessons/interfaces/src/index.ts
+++ lessons/interfaces/src/index.ts
@@ -1,6 +1,57 @@
-import multiply, { multiplyByTwo as mBy2, HelloWorld } from './multiply'
+//Interfaces
+interface Profile {
+    //if name is readonly can't modify directly
+    readonly name: string;
+    //? makes it optional.
+    age?: number;
+}
 
-const a = 5;
-const b = 9;
+let profile: Profile = {
+    name: 'Vaughan',
+    age: 28
+};
 
-console.log(`${a} * ${b} = ${multiply(a, b)}`);
+// profile.name = 'Jim';
+//if name is readonly can't modify directly
+
+// Index Signature
+interface A {
+    someProp: string
+    [key: string]: number | string;
+}
+const a : A = {someProp: 'some prop'};
+a.x = 1;
+a.y = 2;
+
+// Call Signature
+interface Sum {
+    (a: number, b: number): number;
+    prop1: string;
+}
+
+const sum: Sum = (a, b) => a + b;
+sum.prop1 = 'some prop';
+
+// Extending interfaces
+
+interface Parent {
+    x: string;
+}
+
+interface Parent2 {
+    y: string;
+}
+
+interface Parent3 {
+    z: string;
+}
+
+interface Child extends Parent,Parent2,Parent3 {
+
+}
+
+let child: Child = {
+    x: 'some prop',
+    y: 'some prop',
+    z: 'some prop'
+};
