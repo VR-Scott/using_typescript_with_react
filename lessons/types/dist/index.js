d5d552f49f6506cc5a473d9587e7c0fa901e6da3
VR-Scott <vscott@student.wethinkcode.co.za>
2020-04-12 15:11:53 +0200

    completed types

--- lessons/types/dist/index.js
+++ lessons/types/dist/index.js
@@ -1,9 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const multiply_1 = __importDefault(require("./multiply"));
 //types:
 //Boolean:
 let bool = false;
@@ -29,6 +24,41 @@ function uppercaseFirstLetter(str) {
 }
 console.log(uppercaseFirstLetter('hello'));
 uppercaseFirstLetter(null);
-const a = 5;
-const b = 9;
-console.log(`${a} * ${b} = ${multiply_1.default(a, b)}`);
+const myArrObj = [];
+const myObj = {};
+const myMapObj = new Map();
+//Void describes the absence of a value
+function log(message) {
+    console.log(message);
+}
+//this function doesn't return anything thus :void
+//Array represents a list of elements of some type.
+let array1 = ['x', 'y'];
+let array2 = array1;
+//Tuple Array w/ fixed # of elemnts
+let tuple = ['str', 1];
+//Enum way to give more friendly same to set of numbers
+var Colour;
+(function (Colour) {
+    Colour["Red"] = "red";
+    Colour[Colour["Green"] = 5] = "Green";
+    Colour[Colour["Blue"] = 99] = "Blue";
+})(Colour || (Colour = {}));
+let myFavouriteColour = Colour.Blue;
+console.log(Colour.Red, Colour.Green, Colour.Blue);
+console.log(Colour[99]); // if you did Colour['red'] get undefined.
+//Any type of value don't know.
+let Any;
+Any = 'a string';
+Any = 1;
+Any = true;
+//Type assertions like type cast
+const email = document.getElementById('');
+if (email) {
+    email.addEventListener('change', e => {
+        const input = e.currentTarget;
+        //can also but not in .tsx
+        // const input = <HTMLInputElement>e.currentTarget;
+        console.log(input.value);
+    });
+}
--- lessons/types/src/index.ts
+++ lessons/types/src/index.ts
@@ -1,4 +1,3 @@
-import multiply, { multiplyByTwo as mBy2, HelloWorld } from './multiply'
 //types:
 
 //Boolean:
@@ -29,13 +28,58 @@ function uppercaseFirstLetter(str: string | null) {
     }
 }
 
-//Object
-type primitiveTypes = boolean | number | string | symbol | null | undefined; 
-
 console.log(uppercaseFirstLetter('hello'));
 uppercaseFirstLetter(null);
 
-const a = 5;
-const b = 9;
+//Object can be any non-primitive types
+// like array [], an object {} or map new Map();
+//these are primitive types
+type primitiveTypes = boolean | number | string | symbol | null | undefined;
 
-console.log(`${a} * ${b} = ${multiply(a, b)}`);
+const myArrObj : object = [];
+const myObj : object = {};
+const myMapObj : object = new Map();
+
+//Void describes the absence of a value
+function log(message: string): void {
+    console.log(message);
+}
+//this function doesn't return anything thus :void
+
+//Array represents a list of elements of some type.
+let array1: string[] = ['x', 'y'];
+let array2: Array<string> = array1;
+
+//Tuple Array w/ fixed # of elemnts
+let tuple: [string, number] = ['str', 1]
+
+//Enum way to give more friendly same to set of numbers
+enum Colour {
+    Red = 'red',
+    Green = 5,
+    Blue = 99
+}
+
+let myFavouriteColour: Colour = Colour.Blue;
+console.log(Colour.Red, Colour.Green, Colour.Blue);
+console.log(Colour[99]);// if you did Colour['red'] get undefined.
+
+
+//Any type of value don't know.
+let Any: any;
+Any = 'a string';
+Any = 1;
+Any = true;
+
+//Type assertions like type cast
+
+const email = document.getElementById('');
+
+if (email) {
+    email.addEventListener('change', e => {
+        const input = e.currentTarget as HTMLInputElement;
+        //can also but not in .tsx
+        // const input = <HTMLInputElement>e.currentTarget;
+        console.log(input.value);
+    });
+}
