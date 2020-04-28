e7f18b4b23aaf49b2937f513e00c1a8749abf9ad
VR-Scott <vscott@student.wethinkcode.co.za>
2020-04-28 15:10:35 +0200

    finished constructors using interfaces but there is an error

---
+++ lessons/13_constructors_using_interfaces/src/kettle.ts
@@ -0,0 +1,25 @@
+interface Kettle {
+    volume : number;
+}
+
+interface KettleConstructor {
+    new (volume: number): Kettle;
+}
+
+class TeaKettle implements Kettle {
+    volume: number;
+    constructor(volume: number) {}
+}
+
+class ElectricKettle implements Kettle {
+    volume: number;
+    constructor(volume: number) {}
+}
+
+function kettleFactory(KettleClass: KettleConstructor) {
+    const kettle = new KettleClass(2);
+    return kettle;
+}
+
+const teaKettle = kettleFactory(TeaKettle);
+const electricKettle = kettleFactory(ElectricKettle);
