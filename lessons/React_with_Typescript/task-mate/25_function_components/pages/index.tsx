b6b4392e47407f06e505eb04f6ea0e218049f82f
VR-Scott <vscott@student.wethinkcode.co.za>
2020-05-25 19:41:05 +0200

    finished 15_react_function_comp

--- lessons/React_with_Typescript/task-mate/25_function_components/pages/index.tsx
+++ lessons/React_with_Typescript/task-mate/25_function_components/pages/index.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import { NextPage } from 'next';
+import { url } from 'inspector';
 
-interface InitailProps {
-    greeting: string;
+// interface InitailProps {
+//     greeting: string;
+// }
+
+// interface Props extends InitailProps {}
+
+// const IndexPage: NextPage<InitailProps, Props>= props => {
+// return <div className="">{props.greeting}</div>
+// }
+
+// IndexPage.getInitialProps = async () => ({
+//     greeting: 'Hello World!'
+// });
+
+// export default IndexPage; 
+
+interface Task {
+    title: string;
 }
 
-interface Props extends InitailProps {}
-
-const IndexPage: NextPage<InitailProps, Props>= props => {
-return <div className="">{props.greeting}</div>
+interface TasksListProps {
+    tasks: Task[];
 }
 
-IndexPage.getInitialProps = async () => ({
-    greeting: 'Hello World!'
-});
+const TasksList: React.FunctionComponent<TasksListProps> = ({ tasks }) => {
+    return (
+    <ul className="">
+        {tasks.map((task, i) => {
+            return <li key={i} className="">{task.title}</li>;
+        })}
+    </ul>
+    );
+};
 
-export default IndexPage; 
+const tasks = [
+    {title: 'Task 1'},
+    {title: 'Task 2'},
+    {title: 'Task 3'}
+];
+
+export default () => <div className=""><TasksList tasks={tasks} /></div>;
