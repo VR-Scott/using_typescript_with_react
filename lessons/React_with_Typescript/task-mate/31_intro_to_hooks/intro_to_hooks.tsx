451b9999cfefa6e71c59f24ff208b14e5ed07e68
VR-Scott <vscott@student.wethinkcode.co.za>
2020-06-01 16:22:30 +0200

    31

---
+++ lessons/React_with_Typescript/task-mate/31_intro_to_hooks/intro_to_hooks.tsx
@@ -0,0 +1,13 @@
+// don't need classes to use classes to use State, Lifecycle, Refs, Context.
+// instead can use funct + hooks
+
+// Basic Hooks:
+// useState, useEffect, useContext.
+
+// Additional Hooks:
+// useReducer, useCallback, useMemo, useRef
+// useImperitiveHandle, useLayoutEffect, useDebugValue.
+
+// Can combine built in hooks into custom hooks:
+
+// Can't use hooks inside class comps.
