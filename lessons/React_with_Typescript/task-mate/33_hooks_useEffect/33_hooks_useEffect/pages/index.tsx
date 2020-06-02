2ad71f769989784ce7e8adb6463bc7866b88b870
VR-Scott <vscott@student.wethinkcode.co.za>
2020-06-02 16:24:31 +0200

    finished 33_hooks_useEffect

--- lessons/React_with_Typescript/task-mate/33_hooks_useEffect/33_hooks_useEffect/pages/index.tsx
+++ lessons/React_with_Typescript/task-mate/33_hooks_useEffect/33_hooks_useEffect/pages/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 interface InitialProps {
     greeting: string;
@@ -8,7 +9,29 @@ interface InitialProps {
 interface Props extends InitialProps {}
 
 const IndexPage: NextPage<Props, InitialProps> = (props) => {
-return <div className="">{props.greeting}</div>
+    const [likes, setLikes] = useState(0);
+    useEffect(() => {
+        console.log('useEffect callback called')
+        const listener = e => {
+            console.log('mousedown', e.offsetX, e.offsetY);
+        };
+        document.addEventListener('mousedown', listener);
+        return () => {
+            console.log('useEffect CLEANUP funct');
+            document.removeEventListener('mousedown', listener);
+        };
+    }, [likes]);
+    return <div className="">
+        {props.greeting}
+        <p>
+            <Link href={{pathname: '/about'}}>
+                <a>About</a>
+            </Link>
+        </p>
+        <p>
+            {likes} Likes <button onClick={() => setLikes(likes + 1)}>Like</button>
+        </p>
+    </div>
 };
 
 IndexPage.getInitialProps = async () => ({
