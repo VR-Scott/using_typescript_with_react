a3bef78d288eeea7b4395d42702bf6f3fe8302dd
VR-Scott <vscott@student.wethinkcode.co.za>
2020-06-02 12:01:10 +0200

    finished 32_hooks_useState

--- lessons/React_with_Typescript/task-mate/32_hooks_useState/pages/index.tsx
+++ lessons/React_with_Typescript/task-mate/32_hooks_useState/pages/index.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
+import { Interface } from 'readline';
 
 interface InitialProps {}
 
 interface Props extends InitialProps {}
 
+interface State {
+    notificationVisible: boolean;
+}
+
 const IndexPage: NextPage<Props, InitialProps> = () => {
-return <div className="">
-    <div style={{background: 'peachpuff', fontWeight: 'bold'}}>
-        This website uses cookies.<button>I Agree</button>
+    const [state, setState] = useState<State>({
+        notificationVisible: true
+    });
+
+    const handleClick = () => {
+        setState({
+            notificationVisible: false
+        });
+    };
+
+    return <div className="">
+        {state.notificationVisible && (
+            <div
+                style={{ 
+                    background: 'peachpuff',
+                    fontWeight: 'bold',
+                    padding: '10px',
+                    textAlign: 'center'
+                }}
+            >
+                This website uses cookies.{' '}
+                <button onClick={handleClick}>I Agree</button>
+            </div>
+            )}
     </div>
-</div>
 };
 
 export default IndexPage;
